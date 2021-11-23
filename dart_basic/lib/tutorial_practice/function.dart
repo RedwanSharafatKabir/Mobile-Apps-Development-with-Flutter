2605576026530ab9b148077410aabd2c9acb02a6
Redwan Sharafat Kabir <37416018+RedwanSharafatKabir@users.noreply.github.com>
2021-11-24 01:51:54 +0600

    Function

--- dart_basic/lib/tutorial_practice/function.dart
+++ dart_basic/lib/tutorial_practice/function.dart
@@ -1,4 +1,13 @@
 
+void main(List<String> arguments) {
+  print('Hello world !');
+
+  print(sumMethod(5, 3.2));
+  print(sumDoubleMethod(5, 3.2));
+  print(doubleValue(2.5));
+  print(square(2.5));
+}
+
 sumMethod(int m, double n){
   var x = "System";
   return "Sum of m and n is: ${m+n} $x";
@@ -8,9 +17,10 @@ double sumDoubleMethod(int m, double n){
   return m+n;
 }
 
-void main(List<String> arguments) {
-  print('Hello world !');
-
-  print(sumMethod(5, 3.2));
-  print(sumDoubleMethod(5, 3.2));
+// Here double means দ্বিগুণ
+dynamic doubleValue(var num){
+  return num*2;
 }
+
+// => This is called fat arrow / fat arrow expression
+dynamic square(var num) => "Fat Arrow Expression: ${num*num}";
