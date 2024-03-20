2600d7108285300395c15eb1c4fe5dce14157aa3
Redwan Sharafat Kabir <37416018+RedwanSharafatKabir@users.noreply.github.com>
2024-03-20 10:46:31 +0600

    Inheritance

---
+++ Dart Programming/object_oriented_programming/Inheritance_Child.dart
@@ -0,0 +1,30 @@
+
+import 'Inheritance_Parent.dart';
+
+class Student extends Human {
+  String institute;
+  String section;
+
+  /** Line number 9 and 10 are same **/
+  // Student(super.hand, super.leg, this.institute, this.section);
+  Student(int hand, int leg, this.institute, this.section) : super(hand, leg);
+
+  @override
+  void walking([String? name]) {
+    super.walking(); /** It will execute the walking unction of parent class Human **/
+
+    print('Standing with $leg legs');
+
+    print('Student Name: $name');
+  }
+}
+
+void main (){
+  Student studentObj = Student(2, 2, "DIU", "D");
+
+  studentObj.moving();
+  studentObj.walking();
+
+  print('');
+  studentObj.walking("Sharafat");
+}
